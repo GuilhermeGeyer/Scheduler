2a3f8acd963a802c4e59e8a03e54017c5f86b175
Guilherme Geyer <ghiper646@yahoo.com.br>
2023-11-11 15:36:57 -0300

    Formatting

--- Scheduler.py
+++ Scheduler.py
@@ -18,42 +18,47 @@ file_name = 'Scheduler.txt'
 
 
 class Terminal():
-    def __init__(self,data,file_path,file_name):
+    def __init__(self, data, file_path, file_name):
         self.data = data
         self.time = time.time()
         self.file_path = file_path
         self.file_name = file_name
         for i in self.data:
-            #item = self.data.get(i)
-            #item = float(item)
-            self.data.update({i:float(self.data.get(i))})
+            # item = self.data.get(i)
+            # item = float(item)
+            self.data.update({i: float(self.data.get(i))})
             print(f'{i}: {format_time(self.data.get(i))}')
+
     def idle(self):
         while True:
-            input_key = input('key\n(exit to save and quit)\n(merge <old> <new> if misspelled): ')
+            input_key = input(
+                'key\n(exit to save and quit)' +
+                '\n(merge <old> <new> if misspelled): ')
             if input_key == 'exit':
-                save_file(file_path,file_name,data)
+                save_file(file_path, file_name, data)
                 return
-            #print(input_key[:6])
+            # print(input_key[:6])
             elif input_key[:6] == 'merge ':
                 inputs = input_key.split(' ')
-                #print(inputs)
-                self.data.update({inputs[2]:self.data.get(inputs[1]) + self.data.get(inputs[2])})
+                # print(inputs)
+                self.data.update({inputs[2]: self.data.get(
+                    inputs[1]) + self.data.get(inputs[2])})
                 self.data.pop(inputs[1])
-            #input_value = input('value: ')
+            # input_value = input('value: ')
             elif input_key in self.data:
                 current_time = self.data.get(input_key)
-                self.data.update({input_key:current_time + time.time() - self.time})
+                self.data.update(
+                    {input_key: current_time + time.time() - self.time})
             else:
-                self.data.update({input_key:time.time() - self.time})
+                self.data.update({input_key: time.time() - self.time})
             self.time = time.time()
             for i in self.data:
-                #print(f'{i}: {self.data.get(i):.2f}')
+                # print(f'{i}: {self.data.get(i):.2f}')
                 print(f'{i}: {format_time(self.data.get(i))}')
 
 
 def format_time(seconds):
-    #seconds = float(seconds)
+    # seconds = float(seconds)
     minutes = 0
     hours = 0
     if seconds > 3600:
@@ -65,33 +70,33 @@ def format_time(seconds):
     return f'{hours}h {minutes}m {seconds:.2f}s'
 
 
-def clear_file(file_path,file_name):
-    with open(f'{file_path}/{file_name}','w') as file:
+def clear_file(file_path, file_name):
+    with open(f'{file_path}/{file_name}', 'w') as file:
         file.write('{}')
 
 
-def load_file(file_path,file_name):
+def load_file(file_path, file_name):
     try:
-        with open(f'{file_path}/{file_name}','r') as file:
+        with open(f'{file_path}/{file_name}', 'r') as file:
             pass
     except FileNotFoundError:
-        with open(f'{file_path}/{file_name}','w') as file:
+        with open(f'{file_path}/{file_name}', 'w') as file:
             file.write('{}')
     finally:
-        with open(f'{file_path}/{file_name}','r') as file:
+        with open(f'{file_path}/{file_name}', 'r') as file:
             data = json.load(file)
-    #print(f'data: {data} {type(data)}')
+    # print(f'data: {data} {type(data)}')
     return data
 
 
-def save_file(file_path,file_name,data):
-    with open(f'{file_path}/{file_name}','w') as file:
-        json.dump(data,file)
-        #print(data)
+def save_file(file_path, file_name, data):
+    with open(f'{file_path}/{file_name}', 'w') as file:
+        json.dump(data, file)
+        # print(data)
     return data
 
 
-def beep(frequency,duration,sleep_time):
+def beep(frequency, duration, sleep_time):
     timer = time.time()
     winsound.Beep(frequency, duration)
     while time.time() - timer < sleep_time:
@@ -101,22 +106,21 @@ def beep(frequency,duration,sleep_time):
 def idle():
     while True:
         current_time = datetime.datetime.now()
-        alarm(data,current_time)
-        #print(f'{current_time.hour}h{current_time.minute}')
+        alarm(data, current_time)
+        # print(f'{current_time.hour}h{current_time.minute}')
         time.sleep(1)
-    
 
-def alarm(data,current_time):
+
+def alarm(data, current_time):
     current_time_hm = f'{current_time.hour}h{current_time.minute}'
     if current_time_hm in data:
         print(current_time_hm, data.get(current_time_hm))
         while True:
-            beep(1000,200,0.3)
+            beep(1000, 200, 0.3)
 
 
 data = load_file(file_path, file_name)
 
 
-terminal = Terminal(data,file_path,file_name)
+terminal = Terminal(data, file_path, file_name)
 terminal.idle()
-
